2ecb8f569587ff4daef4566a3624633401e5e0bb
Piyush Kumar <piyush.kumar@pristyncare.com>
2022-12-19 17:09:16 +0530

    routing

--- src/components/firstPage/firstPage.js
+++ src/components/firstPage/firstPage.js
@@ -1,7 +1,11 @@
 import styles from "./firstPage.module.css"
 import Header from "./header/header";
 import Body from "./body/body";
-import backgroundImage from "./../../../src/assests/pexels-caio-56759.jpg"
+// import backgroundImage from "./../../../src/assests/pexels-caio-56759.jpg"
+import { Route, Routes } from "react-router-dom";
+import ContactUs from "../headerLinkPages/ContactUsPage/contactUsPage";
+import AboutUS from "../headerLinkPages/AboutUsPage/aboutUsPage";
+import ZasttoCommunity from "../headerLinkPages/ZasttoCommunityPage/zasttoCommunity";
 
 function FirstPage () {
 
@@ -10,7 +14,13 @@ function FirstPage () {
             {/* <img className={styles.imageCustomClass} src={backgroundImage} alt="img"/> */}
             <div style={{ width:"100%", minHeight:"100vh"}}>
                 <Header/>
-                <Body/>
+                <Routes>
+                    <Route path="/" element={<Body/>} />
+                    <Route path="/contact" element={<ContactUs/>} />
+                    <Route path="/about" element={<AboutUS/>} />
+                    <Route path="/community" element={<ZasttoCommunity/>} />
+                </Routes>
+                {/* <Body/> */}
             </div>
         </div>
      );
--- src/components/firstPage/header/navBar/navBar.js
+++ src/components/firstPage/header/navBar/navBar.js
@@ -1,23 +1,43 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { useNavigate } from 'react-router-dom';
 import styles from "./navBar.module.css";
 
 function NavBar() {
+
+    const navigate = useNavigate()
+
+    const redirectToCommunityPage = () => {
+        navigate("/community")
+    }
+
+    const redirectToAboutPage = () => {
+        navigate("/about")
+    }
+
+    const redirectToContactPage = () => {
+        navigate("/contact")
+    }
+
+    const homePageZastto = () => {
+        navigate("/")
+    }
+
     return ( 
         <Navbar expand="lg" bg="transparent" variant="light">
             <Container>
                 <div style={{marginRight:"65%"}}>
-                    <Navbar.Brand><strong className="text-white">Zastto</strong></Navbar.Brand>
+                    <Navbar.Brand><strong className="text-white" style={{cursor:"pointer"}} onClick={homePageZastto}>Zastto</strong></Navbar.Brand>
                 </div>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" className={styles.customNavbarIconClass}>
                 </Navbar.Toggle>
                 <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
                 <Nav className="me-auto">
                    <div className={`${styles.navBarOtherPages} me-4`} >
-                        <span className="text-white" style={{width:"130px"}}>Zastto Community</span>
-                        <span className="text-white" >About</span>
-                        <span className="text-white" >Contact</span>
+                        <span className="text-white" style={{width:"130px"}} onClick={redirectToCommunityPage}>Zastto Community</span>
+                        <span className="text-white" onClick={redirectToAboutPage}>About</span>
+                        <span className="text-white" onClick={redirectToContactPage}>Contact</span>
                     </div>
                 </Nav>
                 </Navbar.Collapse>
